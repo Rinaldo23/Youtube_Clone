9a72e41830bbcb7e9b80203d5097e4919720b49e
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-23 21:38:31 +0530

    Created Comment Model

---
+++ server/models/commentModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.model({
+    userId: {
+        type: String,
+        required: true,
+    },
+    videoId: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+},
+    { timestamps: true }
+);
+
+export default mongoose.model("Comment", commentSchema);
