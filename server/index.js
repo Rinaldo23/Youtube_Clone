dc786a07502e7c4cfd906712797ffe4f0c00fe17
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-23 17:42:02 +0530

    Started Backend

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+// Creating express application
+const app = express();
+
+app.listen(8800,()=>{
+    console.log("Listening on port 8800!");
+});
---
+++ server/notes.md
@@ -0,0 +1,7 @@
+# To create a package.json file
+    npm init -y
+
+# Directly import the module without using require
+    package.json -> "type":"module"
+
+#
