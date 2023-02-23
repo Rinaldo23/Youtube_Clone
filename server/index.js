db538a2fed996740f3f8d52adb7afc9613a335cd
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-23 18:03:45 +0530

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 // Creating express application
 const app = express();
 
-app.listen(8800,()=>{
-    console.log("Listening on port 8800!");
+// To Read .env file
+dotenv.config();
+
+// Creating Connection with Database
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to DB");
+    } catch (err) {
+        throw (err);
+    }
+}
+
+// Connect automatically after any issues || Optional 
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB Disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB Connected");
+});
+
+// Creating a connection to Server
+app.listen(8800, () => {
+    connect();
+    console.log("Connected to Server");
 });
