4f2165984a2e934cf740be3afd279ed63169a4cc
Rinaldo23 <rinaldobadigar@gmail.com>
2023-02-24 18:47:46 +0530

    Get User Fn

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,3 +26,12 @@ export const deleteUser = async (req, res, next) => {
         return next(createError(403, "You can delete only your account!"));
     }
 }
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (err) {
+        next(err);
+    }
+}
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, updateUser } from "../controllers/userController.js";
+import { deleteUser, getUser, updateUser } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const userRouter = express.Router();
@@ -9,5 +9,8 @@ userRouter.put("/:id", verifyToken, updateUser);
 
 // DELETE USER/CHANNEL
 userRouter.delete("/:id", verifyToken, deleteUser);
+
+// GET USER/CHANNEL
+userRouter.get("/find/:id", getUser);
 
 export default userRouter;
